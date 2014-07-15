bb62a36f30089872e75798b3907c79403dced821
Jemacu <maje8691@yahoo.com>
2014-07-14 19:30:08 -0700

    minor fix to python script

--- parsekml.py
+++ parsekml.py
@@ -99,7 +99,6 @@ def folderfound(level,endcnt,f,dpath):
             writefile.write(r"""<?xml version="1.0" encoding="UTF-8"?>
     <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
     <Document>
-    <name>thename</name>
     """)
             writefile.write("<Folder>")
             writefile.write("<name>" + thename + "</name>")
